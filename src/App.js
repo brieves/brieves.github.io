21e8d17ee16f00b3cec5cd41867f7e4c769c8337
davidgarcialand <86802432+davidgarcialand@users.noreply.github.com>
2023-04-11 11:30:13 +0000

    Start commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          O zapateiro Andrés
+        </p>
+        <nav>
+          <ul>
+            <li>Zapatos por encargo</li>
+            <li>Compromiso con la ensenanza</li>
+            <li>Deborah</li>
+            <li>Que pro fino señores</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section id='main-section'>
+          <h1>O zapateiro Andrés</h1>
+          <p>Acercámosche o que che importa</p>
+          <button>Contáctanos</button>
+        </section>
+        <section id='zapatos-encargo'>
+          <h2>Zapatos por encargo</h2>
+          <div>
+            <div></div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
